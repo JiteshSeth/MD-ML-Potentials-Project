062b356f80f16479955ab072b00e1624ff0d0de7
jitesh <jiteshseth369@gmail.com>
2023-01-05 16:45:56 +0530

    Updated input files after changing box dimensions

--- Water_inputscript/inputFiles/water.tcl
+++ Water_inputscript/inputFiles/water.tcl
@@ -1,19 +1,15 @@
-package require solvate
 package require topotools
 
 set molname "waters"
-
-
-#solvate -minmax {{0 0 0} {50 50 50}} -o ${molname}
 
 mol load psf ${molname}.psf pdb ${molname}.pdb
 
 set hyd [atomselect top "type HT"]
 $hyd set type 1HT
-$hyd set charge 0.4238
+$hyd set charge 0.410
 set oxy [atomselect top "type OT"]
 $oxy set type 2OT
-$oxy set charge -0.8476
+$oxy set charge -0.820
 
 topo writelammpsdata water.lmpsys
 
