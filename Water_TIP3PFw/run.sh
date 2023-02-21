9a9b3788f6f65e02a27aafb1c3cfbde6036030bf
jitesh <jiteshseth369@gmail.com>
2023-02-21 14:40:23 +0530

    Adding folder for TIP3P(Flexible)

---
+++ Water_TIP3PFw/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+density="800 850 900 950 995 1000 1005"
+
+# Set the temperatures to run LAMMPS at
+temperatures="300 400 500"
+
+for d in $density 
+
+do
+
+# Loop through the temperatures
+for temp in $temperatures
+do
+    # Run LAMMPS with the current temperature
+    mpirun -np 4 lmp_mpi < water.lmp -var density $d -var temperature $temp
+done
+done 
