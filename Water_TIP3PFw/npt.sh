cd80f4345fa6d0af328edf9b26d3aee7c2f16d32
jitesh <jiteshseth369@gmail.com>
2023-03-17 18:08:29 +0530

    Modifying lmp file for TIP3PFw to run on gpu

--- Water_TIP3PFw/npt.sh
+++ Water_TIP3PFw/npt.sh
@@ -1,7 +1,10 @@
 temperatures="300 400 500"
 
-for temp in $temperatures
-do
-    # Run LAMMPS with the current temperature
-    mpirun -np 4 lmp_mpi -in water.lmp -var temperature $temp
+for irun in 10 11 
+do 
+	for temp in $temperatures
+	do
+	    # Run LAMMPS with the current temperature
+	    mpirun -np 1 lmp_gpu -pk gpu 0 -in water.lmp -var irun $irun -var temperature $temp
+	done
 done
